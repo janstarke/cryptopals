2a7a30f520b93166d6ae889ade01ef52edd8707e
Jan Starke <jan.starke@posteo.de>
2024-04-03 22:41:22 +0200

    fixed challenge 10

--- src/bin/s2c10.rs
+++ src/bin/s2c10.rs
@@ -1,10 +1,14 @@
+use std::fs::File;
+
 use anyhow::Result;
-use cryptopals::{Bytes, PadWith, Pkcs7};
+use cryptopals::{Bytes, Mode, AES, AES_BLOCKSIZE};
 use encoding_rs::WINDOWS_1252;
 
 fn main() -> Result<()> {
-    let mut input = Bytes::from_ascii("YELLOW SUBMARINE");
-    input.pad_with(20, Pkcs7)?;
-    println!("{:?}", input.to_string(WINDOWS_1252).0);
+    let data = Bytes::from_base64_stream(File::open("data/10.txt")?)?;
+    let key = Bytes::from_ascii("YELLOW SUBMARINE").try_into()?;
+    let iv = Bytes::from(vec![0; AES_BLOCKSIZE]).try_into()?;
+    let decrypted = data.aes_cbc(Mode::Decrypt, &key, &iv)?;
+    println!("{}", decrypted.to_string(WINDOWS_1252).0);
     Ok(())
 }

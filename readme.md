c97b8acf702c02e2361122aa6b78bd7aeec49e77
Jan Starke <jan.starke@posteo.de>
2024-04-12 17:47:24 +0200

    solved c12

---
+++ readme.md
@@ -0,0 +1,28 @@
+# My solution to cryptopals
+
+## Set 1
+
+|Challenge #| Title | Status |
+|-|-|-|
+|1|[Convert hex to base64](https://cryptopals.com/sets/1/challenges/1)|[:heavy_check_mark:](src/bin/s1c1.rs)|
+|2|[Fixed XOR](https://cryptopals.com/sets/1/challenges/2)|[:heavy_check_mark:](src/bin/s1c2.rs)|
+|3|[Single-byte XOR cipher](https://cryptopals.com/sets/1/challenges/3)|[:heavy_check_mark:](src/bin/s1c3.rs)|
+|4|[Detect single-character XOR](https://cryptopals.com/sets/1/challenges/4)|[:heavy_check_mark:](src/bin/s1c4.rs)|
+|5|[Implement repeating-key XOR](https://cryptopals.com/sets/1/challenges/5)|[:heavy_check_mark:](src/bin/s1c5.rs)|
+|6|[Break repeating-key XOR](https://cryptopals.com/sets/1/challenges/6)|[:heavy_check_mark:](src/bin/s1c6.rs)|
+|7|[AES in ECB mode](https://cryptopals.com/sets/1/challenges/7)|[:heavy_check_mark:](src/bin/s1c7.rs)|
+|8|[Detect AES in ECB mode](https://cryptopals.com/sets/1/challenges/8)|[:heavy_check_mark:](src/bin/s1c8.rs)|
+
+
+## Set 2
+
+|Challenge #| Title | Status |
+|-|-|-|
+|9|[Implement PKCS#7 padding](https://cryptopals.com/sets/2/challenges/9)|[:heavy_check_mark:](src/bin/s2c9.rs)|
+|10|[Implement CBC mode](https://cryptopals.com/sets/2/challenges/10)|[:heavy_check_mark:](src/bin/s2c10.rs)|
+|11|[An ECB/CBC detection oracle](https://cryptopals.com/sets/2/challenges/11)|[:heavy_check_mark:](src/bin/s2c11.rs)|
+|12|[Byte-at-a-time ECB decryption (Simple)](https://cryptopals.com/sets/2/challenges/12)|[:heavy_check_mark:](src/bin/s2c12.rs)|
+|13|[ECB cut-and-paste](https://cryptopals.com/sets/2/challenges/13)|:x:|
+|14|[Byte-at-a-time ECB decryption (Harder)](https://cryptopals.com/sets/2/challenges/14)|:x:|
+|15|[PKCS#7 padding validation](https://cryptopals.com/sets/2/challenges/15)|:x:|
+|16|[CBC bitflipping attacks](https://cryptopals.com/sets/2/challenges/16)|:x:|
--- src/bin/s2c12.rs
+++ src/bin/s2c12.rs
@@ -1,4 +1,6 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+};
 
 use anyhow::{bail, Result};
 use cryptopals::{encryption_oracle_c12, Bytes};
@@ -6,6 +8,7 @@ use encoding_rs::WINDOWS_1252;
 
 fn main() -> Result<()> {
     let mut block_size = None;
+    let mut plaintext_length = 0;
     for length in 0..20 {
         let data = Bytes::from_ascii(&"A".repeat(length));
         let encrypted = encryption_oracle_c12(&data)?;
@@ -15,7 +18,16 @@ fn main() -> Result<()> {
             Some(bs) => {
                 let new_block_size = encrypted.len();
                 if new_block_size > bs {
+
+                    // we have prefixed the plaintext with enough bytes to force
+                    // the oracle to create a new block. So we know, the plaintext
+                    // length equals the length of the
+                    // ciphertext - blocksize - prefix length
+                    //
+                    // we must subtract blocksize, because if the plaintext length
+                    // is a multiple of blocksize, a padding block is appended
                     block_size = Some(new_block_size - bs);
+                    plaintext_length = encrypted.len() - length - (new_block_size - bs);
                     break;
                 }
             }
@@ -31,42 +43,45 @@ fn main() -> Result<()> {
         &"A".repeat(2 * block_size)
     ))?));
 
-    // decrypt the first block
     let mut known_bytes = Bytes::from(vec![]);
-    for _ in 0..block_size {
-        if let Some(b) = grab_next_byte(block_size, &known_bytes)? {
+    for _ in 0..plaintext_length {
+        if let Some(b) = grab_next_byte(block_size, &known_bytes, plaintext_length)? {
+            //print!("{}", b as char);
             known_bytes.append(b)
         } else {
-            bail!("found no more values");
+            bail!("found no more values ({} missing), found so far: {}",
+            plaintext_length - known_bytes.len(),
+            known_bytes.to_string(WINDOWS_1252).0);
+            //break;
         }
     }
 
-    println!(
-        "{}",
-        known_bytes.to_string(WINDOWS_1252).0
-    );
+    println!("{}", known_bytes.to_string(WINDOWS_1252).0);
 
     Ok(())
 }
 
-fn grab_next_byte(block_size: usize, known_bytes: &Bytes) -> Result<Option<u8>> {
-    if known_bytes.len() >= block_size {
-        bail!("cannot decrypt more than {block_size} bytes");
-    }
+fn grab_next_byte(block_size: usize, known_bytes: &Bytes, _expected_length: usize) -> Result<Option<u8>> {
+    let destined_block = known_bytes.len() / block_size;
+    let known_bytes_in_block = known_bytes.len() % block_size;
+    //let is_last_block = (expected_length - known_bytes.len()) <= block_size;
 
-    let prefix_size = block_size - (known_bytes.len() + 1);
+    let dtblock_range = destined_block * block_size..destined_block * block_size + block_size;
 
+    let prefix_size = block_size - (known_bytes_in_block + 1);
+
+    // this is something like "AAAAAAA"
     let mut prefix = Bytes::from_ascii(&"A".repeat(prefix_size));
     let mut encrypted = encryption_oracle_c12(&prefix)?;
-    encrypted = Bytes::from(&encrypted[0..block_size]);
+    encrypted = Bytes::from(&encrypted[dtblock_range.clone()]);
 
     prefix = &prefix + known_bytes;
-    assert_eq!(prefix.len(), block_size - 1);
+    assert_eq!(prefix.len(), (destined_block + 1) * block_size - 1);
 
     let blocks: HashMap<Bytes, u8> = (0x00..0xff)
         .map(|b| {
             let result = encryption_oracle_c12(&(&prefix + &Bytes::from(vec![b]))).unwrap();
-            (Bytes::from(&result[0..block_size]), b)
+            (Bytes::from(&result[dtblock_range.clone()]), b)
         })
         .collect();
 
